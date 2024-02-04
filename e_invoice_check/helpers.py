ad9796143e1f23c6f75b2350297b616af47e82f3
Robert Sereinig <sereinig.robert@gmail.com>
2024-02-04 20:26:25 +0100

    clean up

--- e_invoice_check/helpers.py
+++ e_invoice_check/helpers.py
@@ -1,4 +1,4 @@
-from saxonche import PySaxonProcessor
+from saxonche import PySaxonProcessor, PySaxonApiError
 from lxml import etree
 from pathlib import Path
 
@@ -6,15 +6,16 @@ from pathlib import Path
 path_to_document_html_files = "e_invoice_check/templates/views/tmp"
 path_to_stylesheets = "e_invoice_check/static/xslt"
 path_to_schemas = "e_invoice_check/static/xsd"
+path_to_uploaded = "e_invoice_check/uploaded/xml"
 
 
 allowed_formats = {
-    "peppol_bis_billing_3.0_invoice":     "Peppol BIS Billing 3.0 ----- Invoice",
+    "peppol_bis_billing_3.0_invoice": "Peppol BIS Billing 3.0 ----- Invoice",
     "peppol_bis_billing_3.0_credit_note": "Peppol BIS Billing 3.0 -- CreditNote",
-    "xrechnung_cii_2.3":                  "XRechnung CII 2.3 ---------- Invoice",
-    "fatturaPA_v1.2":                     "FatturaPA 1.2 (Italy) ------ Invoice",
-    "nav_osa_3.0_invoice":                "NAV OSA 3.0 (Hungary) ------ Invoice",
-    "ksef_eFaktura_2.0":                  "KSeF eFaktur 2.0 (Poland) - Invoice",
+    "xrechnung_cii_2.3": "XRechnung CII 2.3 ---------- Invoice",
+    "fatturaPA_v1.2": "FatturaPA 1.2 (Italy) ------ Invoice",
+    "nav_osa_3.0_invoice": "NAV OSA 3.0 (Hungary) ------ Invoice",
+    "ksef_eFaktura_2.0": "KSeF eFaktur 2.0 (Poland) - Invoice",
 }
 
 format_to_schema_mapping = {
@@ -47,8 +48,10 @@ format_to_schematron_mapping = {
 format_to_xslt_mapping = {
     "peppol_bis_billing_3.0_invoice": ["stylesheet-ubl.xslt"],
     "peppol_bis_billing_3.0_credit_note": ["stylesheet-ubl.xslt"],
-    "xrechnung_cii_2.3": ["xrechnung-2.3.1-xrechnung-visualization-2023-05-12/xsl/cii-xr.xsl",
-                          "xrechnung-2.3.1-xrechnung-visualization-2023-05-12/xsl/xrechnung-html.xsl"],
+    "xrechnung_cii_2.3": [
+        "xrechnung-2.3.1-xrechnung-visualization-2023-05-12/xsl/cii-xr.xsl",
+        "xrechnung-2.3.1-xrechnung-visualization-2023-05-12/xsl/xrechnung-html.xsl",
+    ],
     "fatturaPA_v1.2": ["fatturapa_v1.2.1_de-it.xsl"],
     "nav_osa_3.0_invoice": ["NAV_InvoiceDataTemplate_XSLT_HTML.xslt"],
     "ksef_eFaktura_2.0": ["kseffaktura.xsl"],
@@ -57,25 +60,33 @@ format_to_xslt_mapping = {
 xslt_params = {
     "peppol_bis_billing_3.0_invoice": {"my_param": ""},
     "peppol_bis_billing_3.0_credit_note": {"my_param": ""},
-    "xrechnung_cii_2.3": {"my_param": ""}, 
+    "xrechnung_cii_2.3": {"my_param": ""},
     "fatturaPA_v1.2": {"my_param": ""},
     "nav_osa_3.0_invoice": {"lang": "EN"},
     "ksef_eFaktura_2.0": {"my_param": ""},
 }
 
+curr_exec_dir = {
+    "peppol_bis_billing_3.0_invoice": ".",
+    "peppol_bis_billing_3.0_credit_note": ".",
+    "xrechnung_cii_2.3": "xrechnung-2.3.1-xrechnung-visualization-2023-05-12/xsl/",
+    "fatturaPA_v1.2": ".",
+    "nav_osa_3.0_invoice": ".",
+    "ksef_eFaktura_2.0": ".",
+}
 # instantiate a parser which removes white space while parsing with lxml
 my_parser = etree.XMLParser(remove_blank_text=True)
 
 
-class Xslt_proc:
-    """to instantiate a saxon xslt3.0 processor object."""
+# class Xslt_proc:
+#     """to instantiate a saxon xslt3.0 processor object."""
 
-    proc = PySaxonProcessor(license=False)
-    my_proc = proc.new_xslt30_processor()
+#     proc = PySaxonProcessor(license=False)
+#     my_proc = proc.new_xslt30_processor()
 
-    def __init__(self, stylesheet_text):
-        self.stylesheet_text = stylesheet_text
-        self.xform = self.my_proc.compile_stylesheet(stylesheet_text=stylesheet_text)
+#     def __init__(self, stylesheet_text):
+#         self.stylesheet_text = stylesheet_text
+#         self.xform = self.my_proc.compile_stylesheet(stylesheet_text=stylesheet_text)
 
 
 def validate_file_content(stream, file_ext):
@@ -83,7 +94,8 @@ def validate_file_content(stream, file_ext):
     if file_ext == ".xml":
         try:
             tree = etree.parse(stream, parser=my_parser)
-        except etree.XMLSyntaxError as error:
+
+        except PySaxonApiError as error:
             return (False, 2, error, type(error))
         except Exception as error:
             return (False, 1, error, type(error))
@@ -92,16 +104,28 @@ def validate_file_content(stream, file_ext):
     return (True, 0, None, None)
 
 
-def transform_xml(xslt_proc, input_xml_text, params):
-    """takes an xslt proc object (with loaded stylesheet) and returns a serialized xml"""
-    doc = xslt_proc.proc.parse_xml(xml_text=input_xml_text)
-    for key, value in params.items():
-        print(f"{key}, {value}")
-        xslt_proc.xform.set_parameter(key, xslt_proc.proc.make_string_value(value))
-    out = xslt_proc.xform.transform_to_string(xdm_node=doc)
-    return out
+# def transform_xml(xslt_proc, input_xml_text, params):
+#     """takes an xslt proc object (with loaded stylesheet) and returns a serialized xml"""
+#     doc = xslt_proc.proc.parse_xml(xml_text=input_xml_text)
+#     for key, value in params.items():
+#         print(f"{key}, {value}")
+#         xslt_proc.xform.set_parameter(key, xslt_proc.proc.make_string_value(value))
+#     out = xslt_proc.xform.transform_to_string(xdm_node=doc)
+#     return out
 
-def transform_xdm_nodes(xsltproc, stylesheet_objs, input_obj, cwd="", params="{}"):
+
+def evaluate_xpath(proc, xpathproc, xml_string, xpath_expression, namespaces={}):
+    xml_obj = proc.parse_xml(xml_text=xml_string)
+    xpathproc.set_context(xdm_item=xml_obj)
+    xpathproc.set_backwards_compatible(True)
+    for key, value in namespaces.items():
+        xpathproc.declare_namespace(key, value)
+    return xpathproc.evaluate(xpath_expression)
+
+
+def transform_xdm_nodes_to_string(
+    proc, xsltproc, stylesheet_objs, input_obj, cwd="", params={}
+):
     xsltproc.clear_parameters()
     xsltproc.set_cwd(cwd)
 
@@ -109,10 +133,84 @@ def transform_xdm_nodes(xsltproc, stylesheet_objs, input_obj, cwd="", params="{}
         executable = xsltproc.compile_stylesheet(stylesheet_node=stylesheet)
         if len(params) != 0:
             for key, value in params.items():
-                executable.set_parameter(key, value)
-        result_obj = executable.apply_templates_returning_value(xdm_node=input_obj)
-        input_obj = result_obj
-    return result_obj
+                executable.set_parameter(key, proc.make_string_value(value))
+        result_string = executable.transform_to_string(xdm_node=input_obj)
+        if len(stylesheet_objs) > 1 and i != len(stylesheet_objs) - 1:
+            print(result_string)
+            input_obj = proc.parse_xml(xml_text=result_string)
+    return result_string
+
+
+def validate_with_schematron(proc, xsltproc, input_obj, current_format):
+    stylesheets = []
+    current_xslt_file = (
+        path_to_stylesheets + "/" + format_to_schematron_xslt_mapping[current_format]
+    )
+
+    stylesheet_obj = proc.parse_xml(xml_file_name=current_xslt_file)
+    stylesheets.append(stylesheet_obj)
+
+    return transform_xdm_nodes_to_string(proc, xsltproc, stylesheets, input_obj)
+
+
+def evaluate_svrl_result(proc, xpathproc, schematron_result_string, current_format):
+    namespaces = {"svrl": "http://purl.oclc.org/dsdl/svrl"}
+    xpath_expression = "//svrl:failed-assert"
+    result = evaluate_xpath(
+        proc,
+        xpathproc,
+        schematron_result_string,
+        xpath_expression,
+        namespaces=namespaces,
+    )
+
+    schematron_validation_log = []
+    if not result:
+        schematron_validation_log = [
+            f"Validation with schema: "
+            + format_to_schematron_mapping[current_format]
+            + " was successful.",
+        ]
+        is_schematron_validation_ok = True
+    else:
+        for error in result:
+            xpath_expression = "concat(svrl:failed-assert/@flag, ', ', \
+                                       svrl:failed-assert/@id, '\n', \
+                                       svrl:failed-assert/svrl:text/text(), '\n', \
+                                       'local-name-path: \n', \
+                                       replace(//svrl:failed-assert/@location, 'Q\{[^}]*\}' ,''))"
+            error_str = evaluate_xpath(
+                proc,
+                xpathproc,
+                str(error),
+                xpath_expression,
+                namespaces=namespaces,
+            )
+
+            schematron_validation_log.append(error_str)
+            is_schematron_validation_ok = False
+    return [schematron_validation_log, is_schematron_validation_ok]
+
+
+def validate_with_schema(proc, xsltproc, input_obj, current_format):
+    current_xsd_file = path_to_schemas + "/" + format_to_schema_mapping[current_format]
+    schema = etree.parse(current_xsd_file, parser=my_parser)
+    tree = etree.fromstring(str(input_obj), parser=my_parser)
+
+    #### Validate input file with corresponding xml schema
+    result = get_errors_from_schema_validation(tree, schema)
+    if result is None:
+        schema_validation_log = [
+            f"Validation with schema: "
+            + format_to_schema_mapping[current_format]
+            + " was successful.",
+        ]
+        is_schema_validation_ok = True
+    else:
+        schema_validation_log = result
+        is_schema_validation_ok = False
+    return [schema_validation_log, is_schema_validation_ok]
+
 
 def get_errors_from_schema_validation(xml, schema):
     """validates against the schema"""
@@ -122,3 +220,18 @@ def get_errors_from_schema_validation(xml, schema):
     except AssertionError as e:
         return schema_validator.error_log
 
+
+def create_html_of_uploaded_file(proc, xsltproc, input_obj, current_format):
+    stylesheets = []
+    for stylesheet in format_to_xslt_mapping[current_format]:
+        current_xslt_file = path_to_stylesheets + "/" + stylesheet
+
+        stylesheet_obj = proc.parse_xml(xml_file_name=current_xslt_file)
+        stylesheets.append(stylesheet_obj)
+
+    params = xslt_params[current_format]
+    cwd = curr_exec_dir[current_format]
+    document_html = transform_xdm_nodes_to_string(
+        proc, xsltproc, stylesheets, input_obj, cwd=cwd, params=params
+    )
+    return document_html
--- e_invoice_check/views.py
+++ e_invoice_check/views.py
@@ -1,32 +1,28 @@
+import shutil
 import os
-import re
 import uuid
 from flask import (
     Blueprint,
     flash,
     render_template,
     request,
-    abort,
     current_app,
     url_for,
 )
 from werkzeug.utils import secure_filename
+from saxonche import PySaxonProcessor
 from lxml import etree
 from e_invoice_check.helpers import (
-    get_errors_from_schema_validation,
     validate_file_content,
-    transform_xml,
-    Xslt_proc,
     my_parser,
     path_to_document_html_files,
-    path_to_schemas,
-    path_to_stylesheets,
+    path_to_uploaded,
     allowed_formats,
-    format_to_schema_mapping,
     format_to_schematron_xslt_mapping,
-    format_to_schematron_mapping,
-    format_to_xslt_mapping,
-    xslt_params,
+    evaluate_svrl_result,
+    create_html_of_uploaded_file,
+    validate_with_schematron,
+    validate_with_schema,
 )
 
 
@@ -34,17 +30,12 @@ bp = Blueprint("views", __name__)
 
 
 # TODO:
-# - clean up home view. The render_template should be called only once (continue?).
-#
 # - fix readable html view, height and width issue (100% doesn't work)
-#
-#
-#
-#
-# what I dont like is that I use saxonche and lxml
-# I would prefer to have only one (is this possible)
-# - saxonche -> xslt3.0 (for xml to html, schematron)
-# - lxml -> parsing and schema validation
+# - why when returning xdm node strange results (schematron and html)?
+# - add html to pdf feature
+# - make better code view in html (formatting, highlighting, focus)
+# - move dicts to db
+# - add login and user interface
 
 
 @bp.app_errorhandler(404)
@@ -86,151 +77,103 @@ def home(filename="", document_html_url=""):
     # get object from uploaded file
     if request.files.getlist("file"):
         for uploaded_file in request.files.getlist("file"):
+            ###################################################################
+            #                        check uploaded file                      #
+            ###################################################################
             filename = uploaded_file.filename
-            # Check if file was provided
+            #### Check if file was provided
             if filename == "":
-                return render_template(
-                    "views/home.html", allowed_formats=allowed_formats
-                )
+                break
 
-            # Check file extension
+            #### Check file extension
             file_ext = os.path.splitext(filename)[1]
             if file_ext not in current_app.config["UPLOAD_EXTENSIONS"]:
                 flash(
                     f"File extension not allowed. Allowed values: 'xml', Filename: {filename}"
                 )
-                return render_template(
-                    "views/home.html", allowed_formats=allowed_formats
-                )
+                break
 
-            # Validate file content
+            #### Validate file content
             result = validate_file_content(uploaded_file.stream, file_ext)
             if not result[0]:
                 flash(f"File content error: {result[2]}")
-                return render_template(
-                    "views/home.html", allowed_formats=allowed_formats
-                )
+                break
 
-            # Reset file stream, since it was consumed by file content validation
+            #### Reset file stream, since it was consumed by file content validation
             uploaded_file.stream.seek(0)
-            # Parse xml and transform to str
+            my_uuid = str(uuid.uuid4())
+            proc = PySaxonProcessor(license=False)
+            xsltproc = proc.new_xslt30_processor()
+            xpathproc = proc.new_xpath_processor()
+
+            uploaded_file_name = path_to_uploaded + "/" + my_uuid + ".xml"
+            with open(uploaded_file_name, "wb") as file:
+                shutil.copyfileobj(uploaded_file.stream, file)
+
+            input_obj = proc.parse_xml(xml_file_name=uploaded_file_name)
+
+            #### Parse xml and transform to str
+            uploaded_file.stream.seek(0)
             tree = etree.parse(uploaded_file.stream, parser=my_parser)
             xml = etree.tostring(tree, pretty_print="True", encoding="unicode")
             xml_test = xml
 
-            # Get target format from dropdown
+            #### Get target format from dropdown
             current_format = request.form.get("format-dropdown")
-            # Get and load schema file based on format
-            current_xsd_file = (
-                path_to_schemas + "/" + format_to_schema_mapping[current_format]
+
+            ###################################################################
+            #                     validation starts here                      #
+            ###################################################################
+            validation_report = {}
+
+            #### apply xml schema
+            schema_result = validate_with_schema(
+                proc, xsltproc, input_obj, current_format
             )
-            schema = etree.parse(current_xsd_file, parser=my_parser)
 
-            # validation phase starts here
-            validation_report={}
-            # Validate input file with corresponding xml schema
-            result = get_errors_from_schema_validation(tree, schema)
-            if result is None:
-                schema_validation_log =  [
-                    f"Validation with schema: "
-                    + format_to_schema_mapping[current_format]
-                    + " was successful.",]
-                is_schema_validation_ok = True
-            else:
-                schema_validation_log = result
-                is_schema_validation_ok = False
-
-            validation_report["schema_validation_log"] = schema_validation_log
-            validation_report["is_schema_validation_ok"] = is_schema_validation_ok
+            validation_report["schema_validation_log"] = schema_result[0]
+            validation_report["is_schema_validation_ok"] = schema_result[1]
             validation_report["schema_validation_done"] = True
 
-            # validation with schematron xslt
-            if is_schema_validation_ok == True and \
-               format_to_schematron_xslt_mapping[current_format] is not None:
-                current_xslt_file = (
-                    path_to_stylesheets + "/" + format_to_schematron_xslt_mapping[current_format]
+            is_schema_validation_ok = validation_report["is_schema_validation_ok"]
+
+            ###################################################################
+            #                 apply schematron (if we should)                 #
+            ###################################################################
+            if (
+                is_schema_validation_ok == True
+                and format_to_schematron_xslt_mapping[current_format] is not None
+            ):
+                #### validate with schematron xslt
+                schematron_result = validate_with_schematron(
+                    proc, xsltproc, input_obj, current_format
                 )
-                stylesheet = etree.parse(current_xslt_file, parser=my_parser)
-                xslt = etree.tostring(stylesheet, pretty_print="True", encoding="unicode")
+                #### evaluate the result
+                schematron_val_result = evaluate_svrl_result(
+                    proc, xpathproc, schematron_result, current_format
+                )
 
-                xslt_proc = Xslt_proc(stylesheet_text=xslt)
-                schematron_result = transform_xml(xslt_proc, xml, xslt_params[current_format])
-
-                # Encode the XML string to bytes
-                schematron_result_bytes = schematron_result.encode('utf-8')
-                # Parse the XML string
-                schematron_result_tree = etree.fromstring(schematron_result_bytes)
-
-                # Define the namespace map
-                namespace_map = {'svrl': 'http://purl.oclc.org/dsdl/svrl'}
-
-                # Use xpath with the namespace to select elements
-                result = schematron_result_tree.xpath('//svrl:failed-assert', namespaces=namespace_map)                
-                schematron_validation_log = []
-                if not result:
-                    schematron_validation_log =  [
-                        f"Validation with schema: "
-                        + format_to_schematron_mapping[current_format]
-                        + " was successful.",]
-                    is_schematron_validation_ok = True
-                    flash(f" schematron result ok")
-                else:
-                    for error in result:
-                        error_type = error.get("flag")         
-                        error_id = error.get("id")         
-                        error_text = error.find("./*[1]").text
-                        error_location_raw = error.get("location")         
-
-                        pattern_to_find = r"Q\{[^}]*\}"
-                        error_location = re.sub(pattern_to_find, "", error_location_raw)
-                        error_str = f"{error_type}, " + \
-                                    f"{error_id} \n" + \
-                                    f"{error_text} \n" + \
-                                    f"{error_location}" 
-
-                        schematron_validation_log.append(error_str)
-                        is_schematron_validation_ok = False
-
-                validation_report["schematron_validation_log"] = schematron_validation_log
-                validation_report["is_schematron_validation_ok"] = is_schematron_validation_ok
+                validation_report["schematron_validation_log"] = schematron_val_result[
+                    0
+                ]
+                validation_report[
+                    "is_schematron_validation_ok"
+                ] = schematron_val_result[1]
                 validation_report["schematron_validation_done"] = True
-                    
-            # Create html view of the document with xslt3 and save to template dir
 
-            current_xslt_file = (
-                path_to_stylesheets + "/" + format_to_xslt_mapping[current_format][0]
+            ###################################################################
+            #                Create html view of the document                 #
+            ###################################################################
+
+            document_html = create_html_of_uploaded_file(
+                proc, xsltproc, input_obj, current_format
             )
-            stylesheet = etree.parse(current_xslt_file, parser=my_parser)
-            xslt = etree.tostring(stylesheet, pretty_print="True", encoding="unicode")
-
-            xslt_proc = Xslt_proc(stylesheet_text=xslt)
-
-            if len(format_to_xslt_mapping[current_format]) > 1:
-                # for some formats a two step transformation is required
-                pass
-                # from intermediate transform to final html
-                intermediate_format = transform_xml(xslt_proc, xml, xslt_params[current_format])
-
-
-                current_xslt_file = (
-                    path_to_stylesheets + "/" + format_to_xslt_mapping[current_format][1]
-                )
-                stylesheet = etree.parse(current_xslt_file, parser=my_parser)
-                xslt = etree.tostring(stylesheet, pretty_print="True", encoding="unicode")
-
-                xslt_proc2 = Xslt_proc(stylesheet_text=xslt)
-
-
-                # document_html = transform_xml(xslt_proc2, intermediate_format, xslt_params[current_format])
-
-            else:
-                document_html = transform_xml(xslt_proc, xml, xslt_params[current_format])
-
-            document_html_name = str(uuid.uuid4()) + ".html"
+            #### save html to file
+            document_html_name = my_uuid + ".html"
             with open(
                 f"{path_to_document_html_files}/{document_html_name}", "w"
             ) as document_html_file:
-                document_html_file.write(document_html)
+                document_html_file.write(str(document_html))
 
             document_html_url = url_for(
                 "views.document", document_html_name=document_html_name
@@ -242,7 +185,7 @@ def home(filename="", document_html_url=""):
                 document_html_url=document_html_url,
                 allowed_formats=allowed_formats,
                 validation_report=validation_report,
-                xml_test=xml_test
+                xml_test=xml_test,
             )
 
     return render_template("views/home.html", allowed_formats=allowed_formats)
